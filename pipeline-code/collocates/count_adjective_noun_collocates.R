f2f6580185d6c354958c653287743786e9fe5ca5
stephbuon <steph.buon@gmail.com>
2021-10-31 19:21:54 -0500

    stuff

--- pipeline-code/collocates/count_adjective_noun_collocates.R
+++ pipeline-code/collocates/count_adjective_noun_collocates.R
@@ -10,4 +10,11 @@ adjective_noun_collocates <- adjective_noun_collocates %>%
 adjective_noun_collocates <- adjective_noun_collocates %>%
   count(grammatical_collocates, afinn, textblob, vader, decade)
 
+adjective_noun_collocates <- adjective_noun_collocates %>%
+  filter(!str_detect(grammatical_collocates, "^[:punct:] "))
+
+adjective_noun_collocates$grammatical_collocates <- str_replace(adjective_noun_collocates$grammatical_collocates, "[:punct:]", "")
+
+adjective_noun_collocates$grammatical_collocates <- str_to_title(adjective_noun_collocates$grammatical_collocates)
+
 write_csv(adjective_noun_collocates, "~/clean_all_adjective_noun_collocates.csv")
